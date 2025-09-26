4a9782b32c111967a9b4dcb2821f2284a8c7c05e
AnandBuisness0023 <buisnessanand4957@gmail.com>
2025-09-26 17:58:19 +0530

    Updated feature / fixed bug / new UI changes

--- src/app/api/inngest/route.js
+++ src/app/api/inngest/route.js
@@ -1,13 +1,13 @@
-import { serve } from "inngest/next";
-import { inngest } from "@/config/inngest";
-import { syncUserCreation, syncUserDeletion, syncUserupdation } from "../../config/inngest";
+// import { serve } from "inngest/next";
+// import { inngest } from "@/config/inngest";
+// import { syncUserCreation, syncUserDeletion, syncUserupdation } from "../../config/inngest";
 
-// Create an API that serves zero functions
-export const { GET, POST, PUT } = serve({
-  client: inngest,
-  functions: [
-    syncUserCreation,
-    syncUserDeletion,
-    syncUserupdation
-  ],
-});
+// // Create an API that serves zero functions
+// export const { GET, POST, PUT } = serve({
+//   client: inngest,
+//   functions: [
+//     syncUserCreation,
+//     syncUserDeletion,
+//     syncUserupdation
+//   ],
+// });
